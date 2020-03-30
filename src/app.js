1b45ab7d92970cd8ad868336a0d39f16bed3f76f
HegerValesin <hegervfp@gmail.com>
2020-03-30 17:29:40 -0300

    comando log

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(express.urlencoded({ extended: true}));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(errors());
 
 //app.listen(3333);
