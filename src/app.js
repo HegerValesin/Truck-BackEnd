c83dd37d2588d35f949878dbb02b39ef5bacea77
HegerValesin <hegervfp@gmail.com>
2020-03-30 17:16:13 -0300

    comando log

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('express');
+const morgan = require('morgan');
 const cors = require('cors'); 
 const { errors } = require('celebrate');
 const routes = require('./routes');
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-//app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true}));
+app.use(morgan('dev'));
 app.use(errors());
 
 //app.listen(3333);
